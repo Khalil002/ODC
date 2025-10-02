223672685a2c67f12e1692832d7d3615aa755d70
Khalil El Hage Kassem <90007227+Khalil002@users.noreply.github.com>
2025-10-02 03:16:58 +0200

    Create finder.py

---
+++ c7/finder.py
@@ -0,0 +1,14 @@
+import os
+import subprocess
+
+# Basic usage
+
+
+
+os.environ["PYTHONUNBUFFERED"] = "1"
+result = subprocess.run(['python3', 'c7/buffertest.py'], 
+                        env={'PYTHONUNBUFFERED': '1'},
+                        capture_output=True, text=True)
+print("Return code:", result.returncode)
+print("Output:", result.stdout)
+print("Errors:", result.stderr)
