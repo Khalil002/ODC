bd10e516d05c9f95dc10ae751556416bdb88803a
Khalil El Hage Kassem <90007227+Khalil002@users.noreply.github.com>
2025-10-02 03:17:25 +0200

    Update finder.py

--- c7/finder.py
+++ c7/finder.py
@@ -6,7 +6,7 @@ import subprocess
 
 
 os.environ["PYTHONUNBUFFERED"] = "1"
-result = subprocess.run(['python3', 'c7/buffertest.py'], 
+result = subprocess.run(['python3', 'c7/wrapper.py'], 
                         env={'PYTHONUNBUFFERED': '1'},
                         capture_output=True, text=True)
 print("Return code:", result.returncode)
